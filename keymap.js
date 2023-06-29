8ffb1060e7c7fc8988eec49970b2b22d0f0e6e98
John Garland <johnpg82@gmail.com>
2023-06-29 09:47:16 -0600

    Create keymap.js

---
+++ keymap.js
@@ -0,0 +1,109 @@
+class KeyboardMapper {
+  constructor() {
+    this.keyMappings = new Map();
+    this.activeKeys = new Set();
+    this.ignoreInputs = ["INPUT", "SELECT", "TEXTAREA"];
+    this.scope = "default";
+
+    //this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+
+    document.addEventListener("keydown", this.handleKeyDown.bind(this), true);
+    document.addEventListener("keyup", this.handleKeyUp);
+  }
+
+  mapCombination(combination, callback, scope = "default") {
+    if (typeof combination !== "string" || typeof callback !== "function") {
+      console.error("Invalid combination or callback");
+      return;
+    }
+
+    const keyCombination = combination.toLowerCase();
+    if (!this.keyMappings.has(keyCombination)) {
+      this.keyMappings.set(keyCombination, []);
+    }
+
+    const mappings = this.keyMappings.get(keyCombination);
+    mappings.push({ callback, scope });
+
+    // Attach event listener for keydown
+    document.addEventListener("keydown", (event) => {
+      const pressedKey = this.getKeyFromEvent(event).toLowerCase();
+      const activeScope = this.getScope();
+
+      mappings.forEach((mapping) => {
+        if (
+          (mapping.scope === activeScope || mapping.scope == "default") &&
+          pressedKey === keyCombination
+        ) {
+          event.preventDefault(); // Prevent default browser behavior
+          mapping.callback(event);
+        }
+      });
+    });
+  }
+
+  getScope() {
+    return this.scope;
+  }
+
+  unmapCombination(combination) {
+    const keyCombination = combination.toLowerCase();
+    this.keyMappings.delete(keyCombination);
+  }
+
+  handleKeyDown(event) {
+    if (this.ignoreInputs.includes(event.target.tagName)) return;
+
+    const key = this.getKeyFromEvent(event);
+    this.activeKeys.add(key);
+
+    this.executeCallbacks();
+  }
+
+  handleKeyUp(event) {
+    if (this.ignoreInputs.includes(event.target.tagName)) return;
+
+    const key = this.getKeyFromEvent(event);
+    this.activeKeys.delete(key);
+  }
+
+  executeCallbacks() {
+    const activeCombination = Array.from(this.activeKeys).join("+");
+    const mappings = this.keyMappings.get(activeCombination);
+
+    if (mappings) {
+      mappings.forEach((mapping) => {
+        const { callback, scope } = mapping;
+        if (typeof callback === "function") {
+          const currentScope = this.getScope();
+          if (currentScope === scope || scope === "default") {
+            callback();
+          }
+        }
+      });
+    }
+  }
+
+  getKeyFromEvent(event) {
+    const { key, shiftKey, ctrlKey, altKey, metaKey } = event;
+    let result = "";
+
+    if (ctrlKey || metaKey) result += "ctrl+";
+    if (shiftKey) result += "shift+";
+    if (altKey) result += "alt+";
+
+    result += key.toLowerCase();
+
+    return result;
+  }
+
+  setScope(scope) {
+    this.scope = scope;
+  }
+
+  destroy() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+    document.removeEventListener("keyup", this.handleKeyUp);
+  }
+}
